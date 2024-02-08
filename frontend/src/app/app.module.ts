dc12e3f426014f9408ff1927c63f8bfd5d5f96ff
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-02-08 11:31:54 +0000

    login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProductComponent } from './product/product.component';
-import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ProductComponent } from './pages/product/product.component';
+import { SearchComponent } from './pages/search/search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
 import { CarouselHomeComponent } from './components/carousel-home/carousel-home.component';
-import { ContactusComponent } from './contactus/contactus.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
