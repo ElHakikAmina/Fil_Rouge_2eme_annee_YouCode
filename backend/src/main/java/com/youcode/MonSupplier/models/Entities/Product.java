20f9cbe09f3a1d84ca133fccc66948df170f44ae
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-13 16:15:44 +0000

    login register

--- backend/src/main/java/com/youcode/MonSupplier/models/Entities/Product.java
+++ backend/src/main/java/com/youcode/MonSupplier/models/Entities/Product.java
@@ -1,4 +1,20 @@
 package com.youcode.MonSupplier.models.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private float prix;
+    private int less_quantity;
+    private int quantity;
+    private String description;
+    id_category ;
+        id_supplier;
 }
