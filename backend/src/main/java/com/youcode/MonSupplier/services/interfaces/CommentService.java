0143979c56ce7caed1e0079cd10467578a77b545
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-18 13:22:56 +0000

    comment service

---
+++ backend/src/main/java/com/youcode/MonSupplier/services/interfaces/CommentService.java
@@ -0,0 +1,18 @@
+package com.youcode.MonSupplier.services.interfaces;
+
+import com.youcode.MonSupplier.models.Dtos.CommentDto.CommentDto;
+import com.youcode.MonSupplier.models.Entities.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentService {
+    List<Comment> getCategories();
+
+    Comment CreateComment(CommentDto commentDto);
+    Optional<Comment> ReadCommentById(Long id);
+
+    Comment UpdateComment(CommentDto commentDto);
+
+    Boolean DeleteComment(Long id);
+}
