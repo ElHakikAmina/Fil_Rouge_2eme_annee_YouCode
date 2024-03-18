c400bebd5ef1bbd8247ab5fd449431324b4b4bc2
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-18 13:12:42 +0000

    controller for the comment

---
+++ backend/src/main/java/com/youcode/MonSupplier/controllers/CommentController.java
@@ -0,0 +1,41 @@
+package com.youcode.MonSupplier.controllers;
+
+import com.youcode.MonSupplier.models.Dtos.CommentDto.CommentDto;
+import com.youcode.MonSupplier.services.interfaces.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Controller
+@CrossOrigin("*")
+@RequestMapping("api/Comment")
+public class CommentController {
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/getAllComments")
+    public Object getAllComments(){
+        return commentService.getComments();
+    }
+    @GetMapping("/getComment/{id}")
+    public Object getComment(@PathVariable Long id){
+         return commentService.ReadCommentById(id);
+    }
+    @PostMapping("/createComment")
+    public Object createComment(@RequestBody CommentDto comment){
+        return commentService.CreateComment(comment);
+    }
+    @PutMapping("/updateComment")
+    public Object updateComment(@RequestBody CommentDto comment){
+        return commentService.UpdateComment(comment);
+    }
+    @DeleteMapping("/deleteComment/{id}")
+    public boolean deleteComment(@PathVariable Long id){
+        return commentService.DeleteComment(id);
+    }
+}
