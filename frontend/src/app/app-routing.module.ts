aa3e14335b9c21f182f579b9dd35619b1416f24a
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-20 14:04:18 +0000

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductComponent } from './pages/product/product.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: '', component:HomeComponent},
-  {path : 'product/:id' , component:ProductComponent}
+  {path : 'product/:id' , component:ProductComponent},
+  {path: 'contactus' , component:ContactusComponent},
+  {path: 'search' , component:SearchComponent},
+  {path: 'signup' , component:SignupComponent}
 ];
 
 @NgModule({
