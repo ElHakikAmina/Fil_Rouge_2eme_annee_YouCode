c460a963917c8ec17a044629fd3092cbdca0d648
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-22 22:50:22 +0000

    login style

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -11,8 +11,8 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   user: any ={
-    email : String,
-    password : String
+    email : '',
+    password : ''
   };
 
   constructor (private userService:UserService  , private router: Router){ }
@@ -49,38 +49,4 @@ export class LoginComponent implements OnInit {
 
 
 
-//   login(user: any) {
-//     this.userService.Login(user)
-//       .subscribe({
-//         next: data => {
-//           // Extract user ID and role from the response
-//           //const userId = data.id;
-//           const userRole = data.role;
-// console.log(userRole);
-//           // Store user ID and role in local storage
-//          // localStorage.setItem('userId', userId.toString());
-//           localStorage.setItem('userRole', userRole);
-
-//           // Navigate to the desired page after successful login
-//           // For example, navigate to the home page
-//            // Determine which dashboard to navigate based on the user's role
-//            let dashboardRoute = '/'; // Default route for unknown roles
-//            if (userRole === 'Admin') {
-//              dashboardRoute = '/dashboardadmin';
-//            } else if (userRole === 'Supplier') {
-//              dashboardRoute = '/dashboardsupplier';
-//            } else if (userRole === 'Buyer') {
-//              dashboardRoute = '/dashboardbuyer';
-//            }
- 
-//            // Navigate to the appropriate dashboard page after successful login
-//            this.router.navigate([dashboardRoute]);
-//         },
-//         error: err => {
-//           console.error(err);
-//           // Handle login error
-//           // For example, display an error message
-//         }
-//       });
-//     }
 }
