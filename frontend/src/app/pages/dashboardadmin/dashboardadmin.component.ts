b8ba591e619560739aa701d11e5be021a2d1ef80
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-22 19:38:18 +0000

    show categories

--- frontend/src/app/pages/dashboardadmin/dashboardadmin.component.ts
+++ frontend/src/app/pages/dashboardadmin/dashboardadmin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../Services/Admin/admin.service';
-
+import { CategoryService } from '../../Services/Category/category.service';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-dashboardadmin',
@@ -10,11 +11,15 @@ import { AdminService } from '../../Services/Admin/admin.service';
 
 export class DashboardAdminComponent implements OnInit {
   suppliers: any[] = [];
+  buyers: any[] =[];
+  categories: Category[] = [];
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService, private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.loadSuppliers();
+    this.loadBuyers();
+    this.loadCategories();
   }
 
   loadSuppliers(): void {
@@ -28,4 +33,35 @@ export class DashboardAdminComponent implements OnInit {
       }
     );
   }
+
+ 
+  
+  loadBuyers(): void {
+    this.adminService.getAll('Buyer').subscribe(
+      (data) => {
+        this.buyers = data;
+      },
+      (error) => {
+        console.error('Error fetching buyers:', error);
+        // Handle error, e.g., display an error message to the user
+      }
+    );
+  }
+
+
+  loadCategories(): void {
+    // Method to load categories
+    this.categoryService.getAllCategories().subscribe(
+      (data) => {
+        this.categories = data;
+      },
+      (error) => {
+        console.error('Error fetching categories:', error);
+        // Handle error, e.g., display an error message to the user
+      }
+    );
+  }
+
+
+
 }
