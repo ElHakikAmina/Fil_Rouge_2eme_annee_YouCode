1897d454c01f085e5ea34593517acd3a484239d4
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2024-03-24 21:50:44 +0000

    deconnexion admin

--- frontend/src/app/components/nav-dashboard-admin/nav-dashboard-admin.component.ts
+++ frontend/src/app/components/nav-dashboard-admin/nav-dashboard-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-dashboard-admin',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-dashboard-admin.component.css']
 })
 export class NavDashboardAdminComponent {
+
+  constructor(private router: Router) {}
+
+  deconnexion(): void {
+    // Supprimer les informations de l'utilisateur du stockage local
+    localStorage.removeItem('roleUser');
+    localStorage.removeItem('idUser');
+    
+    // Rediriger vers la page d'accueil ou une autre page
+    this.router.navigate(['/']);
+  }
 
 }
